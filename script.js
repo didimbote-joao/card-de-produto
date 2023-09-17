f7826e1a4a489bcb8dbfadf0fdb2cc542427824a
didimbote-joao <didimbote_joao@outlook.com>
2023-09-17 06:48:50 +0100

    script is working

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-function buttonMode() {
-  alert("Esta funcionando") 
-  // const html = document.documentElement;
+function viewSofa() {
+  alert("Esta funcionando")
+  const html = document.documentElement
 
-  //  html.classList.toggle("sinal")
+  html.classList.toggle("sinal")
 
-  //  const img = document.querySelector("sofa img")
-  //  if (html.classList.contains("sinal")) {
-  //    img.setAttribute("src", "./assets/w-brand.png")
-  //    img.setAttribute("alt", "Windows")
-  //  }
-  //  else{
-  //    img.setAttribute("src", "./assets/image 2.png")
-  //    img.setAttribute("alt", "Sofa Margarot ")
-  //  }
+  alert("Mudanca de modo Esta funcionando")
+
+  const img = document.querySelector("img")
+  alert("Selecao da imagem Esta funcionando")
+  if (html.classList.contains("sinal")) {
+    alert("if Esta funcionando")
+    img.setAttribute("src", "./assets/sofa.gif")
+    img.setAttribute("alt", "sofa girando")
+  } 
+  // else {
+  //   alert("Desativando o if Esta funcionando")
+  //   img.setAttribute("src", "./assets/image 2.png")
+  //   img.setAttribute("alt", "Sofa Margarot ")
+  // }
 }
