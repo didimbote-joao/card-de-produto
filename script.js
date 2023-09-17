cae821ff749be64568a88c315e8fd511407077f9
didimbote-joao <didimbote_joao@outlook.com>
2023-09-17 06:33:57 +0100

    rename the gif

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function buttonMode() {
+  alert("Esta funcionando") 
   // const html = document.documentElement;
 
-  // html.classList.toggle("sinal")
+  //  html.classList.toggle("sinal")
 
-  // const img = document.querySelector("sofa img")
-  // if (html.classList.contains("sinal")) {
-  //   img.setAttribute("src", "./assets/w-brand.png")
-  //   img.setAttribute("alt", "Windows")
-  // }
-  // else{
-  //   img.setAttribute("src", "./assets/image 2.png")
-  //   img.setAttribute("alt", "Sofa Margarot ")
-  // }
-  alert("Esta funcionando")
+  //  const img = document.querySelector("sofa img")
+  //  if (html.classList.contains("sinal")) {
+  //    img.setAttribute("src", "./assets/w-brand.png")
+  //    img.setAttribute("alt", "Windows")
+  //  }
+  //  else{
+  //    img.setAttribute("src", "./assets/image 2.png")
+  //    img.setAttribute("alt", "Sofa Margarot ")
+  //  }
 }
